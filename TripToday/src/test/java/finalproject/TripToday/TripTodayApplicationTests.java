4da8adc430632145e7be4bafcb8fb148cc807e40
unknown <voinescudenis@icloud.com>
2025-03-04 16:56:33 +0200

    Implemented login and logout using Auth0

---
+++ TripToday/src/test/java/finalproject/TripToday/TripTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.TripToday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
