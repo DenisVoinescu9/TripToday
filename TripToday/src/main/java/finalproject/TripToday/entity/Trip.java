74eb7f137a6c0dfa9a68eba7e21ad51af6d6269d
DenisVoinescu9 <voinescudenis@icloud.com>
2025-04-16 15:36:08 +0300

    upcoming trips

---
+++ TripToday/src/main/java/finalproject/TripToday/entity/Trip.java
@@ -0,0 +1,173 @@
+package finalproject.TripToday.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "trips")
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "trips_id_gen")
+    @SequenceGenerator(name = "trips_id_gen", sequenceName = "trips_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "destination", nullable = false)
+    private String destination;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "departure_location", nullable = false)
+    private String departureLocation;
+
+    @NotNull
+    @Column(name = "departure_date", nullable = false)
+    private LocalDate departureDate;
+
+    @NotNull
+    @Column(name = "departure_hour", nullable = false)
+    private LocalTime departureHour;
+
+    @NotNull
+    @Column(name = "return_date", nullable = false)
+    private LocalDate returnDate;
+
+    @NotNull
+    @Column(name = "duration_days", nullable = false)
+    private Integer durationDays;
+
+    @NotNull
+    @Column(name = "available_spots", nullable = false)
+    private Integer availableSpots;
+
+    @Column(name = "registration_fee", precision = 10, scale = 2)
+    private BigDecimal registrationFee;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "guide_id", nullable = false)
+    private String guideId;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+    @Size(max = 255)
+    @Column(name = "picture")
+    private String picture;
+
+    @Size(max = 255)
+    @Column(name = "hotel_name")
+    private String hotelName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureLocation() {
+        return departureLocation;
+    }
+
+    public void setDepartureLocation(String departureLocation) {
+        this.departureLocation = departureLocation;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalTime getDepartureHour() {
+        return departureHour;
+    }
+
+    public void setDepartureHour(LocalTime departureHour) {
+        this.departureHour = departureHour;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Integer getDurationDays() {
+        return durationDays;
+    }
+
+    public void setDurationDays(Integer durationDays) {
+        this.durationDays = durationDays;
+    }
+
+    public Integer getAvailableSpots() {
+        return availableSpots;
+    }
+
+    public void setAvailableSpots(Integer availableSpots) {
+        this.availableSpots = availableSpots;
+    }
+
+    public BigDecimal getRegistrationFee() {
+        return registrationFee;
+    }
+
+    public void setRegistrationFee(BigDecimal registrationFee) {
+        this.registrationFee = registrationFee;
+    }
+
+    public String getGuideId() {
+        return guideId;
+    }
+
+    public void setGuideId(String guideId) {
+        this.guideId = guideId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+}
---
+++ TripToday/src/main/java/finalproject/TripToday/repository/TripRepository.java
@@ -0,0 +1,16 @@
+package finalproject.TripToday.repository;
+
+import finalproject.TripToday.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+    @Query("SELECT t FROM Trip t WHERE t.departureDate >= :currentDate ORDER BY t.departureDate ASC")
+    List<Trip> findUpcomingTrips(@Param("currentDate") LocalDate currentDate);
+}
