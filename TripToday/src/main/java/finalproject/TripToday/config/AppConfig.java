cd7db044af0411d178678aa4c521be9418ebc04a
Denis <voinescudenis@icloud.com>
2025-04-24 07:57:25 -0700

    working everything

--- TripToday/src/main/java/finalproject/TripToday/config/AppConfig.java
+++ TripToday/src/main/java/finalproject/TripToday/config/AppConfig.java
@@ -1,15 +1,27 @@
-package finalproject.TripToday.config;
+package finalproject.TripToday.config; // Sau pachetul tău de configurare
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppConfig {
+public class AppConfig { // Numește clasa cum dorești
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate() {
+        // 1. Creează un client Apache HttpClient
+        //    Poți folosi createDefault() sau îl poți customiza mai mult dacă e nevoie
+        //    (ex: setare timeout-uri, pool de conexiuni etc.)
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        // 2. Creează un Request Factory bazat pe clientul Apache HttpClient
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        // 3. Creează bean-ul RestTemplate folosind acest request factory
+        return new RestTemplate(requestFactory);
     }
 }
