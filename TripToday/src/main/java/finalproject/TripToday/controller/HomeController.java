207da53805d3946dcdecca893e3dd87760e1e5ce
Denis <voinescudenis@icloud.com>
2025-05-10 02:59:45 +0300

    Finished everything

--- TripToday/src/main/java/finalproject/TripToday/controller/HomeController.java
+++ TripToday/src/main/java/finalproject/TripToday/controller/HomeController.java
@@ -49,7 +49,7 @@ public class HomeController {
 
         model.addAttribute("aboutUsHeading", "Discover the world with us");
         model.addAttribute("aboutUsParagraph", "Explore unforgettable destinations with TripToday! We create immersive guided tours led by passionate experts. Our guides share authentic stories and insights, making your journey seamless and memorable. Leave the details to us, enjoy the discovery. Find your next adventure with TripToday!");
-        model.addAttribute("aboutUsImageSrc", "/images/guides.jpg");
+        model.addAttribute("aboutUsImageSrc", "/images/guide_group.png");
         model.addAttribute("aboutUsImageAlt", "Guides group photo");
         model.addAttribute("travelersTestimonialsHeading", "Our travelers' testimonials");
 
@@ -57,8 +57,8 @@ public class HomeController {
 
         List<Testimonial> testimonials = new ArrayList<>();
         testimonials.add(new Testimonial("/images/traveler1.png", "\"Booking our trip through this company was incredibly straightforward. Our guides were so experienced and passionate, making the stunning historical sites truly come alive. A seamless and amazing experience!\""));
-        testimonials.add(new Testimonial("/images/guides.jpg", "\"What a fantastic adventure! Signing up was a breeze online. The guides knew the nature trails inside out and were super friendly. Saw breathtaking landscapes thanks to their expert planning\""));
-        testimonials.add(new Testimonial("/images/guides.jpg", "\"Everything was handled perfectly, from the easy booking to the tour itself. Our guide was exceptionally knowledgeable and made exploring the region fun. Absolutely brilliant service and location!\""));
+        testimonials.add(new Testimonial("/images/traveler2.jpg", "\"What a fantastic adventure! Signing up was a breeze online. The guides knew the nature trails inside out and were super friendly. Saw breathtaking landscapes thanks to their expert planning\""));
+        testimonials.add(new Testimonial("/images/traveler3.jpg", "\"Everything was handled perfectly, from the easy booking to the tour itself. Our guide was exceptionally knowledgeable and made exploring the region fun. Absolutely brilliant service and location!\""));
 
         // Add list of testimonials to the model
 
